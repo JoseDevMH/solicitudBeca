6e27b2a40ed54127e24b477c3b2f5582c1168228
DiegoPizanoL <67017096+DiegoPizanoL@users.noreply.github.com>
2020-06-24 13:51:14 -0500

    insertAplication.php
    Agregar las variables restantes

--- insertAplication.php
+++ insertAplication.php
@@ -1,3 +1,43 @@
 <?php
+    //* ---------------- Información de Alumno -----------------------------------
     echo $nombreAlumno = $_POST['nombreAlumno'];
+    echo $primerApellidoAlumno = $_POST['primerApellidoAlumno'];
+    echo $segundoApellidoAlumno = $_POST['segundoApellidoAlumno'];
+    echo $EdadAlumno = $_POST['EdadAlumno'];
+    echo $CurpAlumno = $_POST['CurpAlumno'];
+    echo $Calle = $_POST['Calle'];
+    echo $Numero = $_POST['Numero'];
+    echo $ColoniaAlumno = $_POST['ColoniaAlumno'];
+    echo $LocalidadAlumno = $_POST['LocalidadAlumno'];
+
+    //* ---------------- Información de la Escuela -----------------------------------
+    echo $Primaria = $_POST['Primaria'];
+    echo $Secundaria = $_POST['Secundaria'];
+    echo $GradoEscolar = $_POST['GradoEscolar'];
+    echo $PromedioAlumno = $_POST['PromedioAlumno'];
+    echo $NombreEscuela = $_POST['NombreEscuela'];
+    echo $LocalidadEscuela = $_POST['LocalidadEscuela'];
+    echo $Matutino = $_POST['Matutino'];
+    echo $Vespertino = $_POST['Vespertino'];
+
+    //* ---------------- Información de los Padres o Tutor-----------------------------------
+    echo $NombreDelPadre = $_POST['NombreDelPadre'];
+    echo $ocupacionPadre = $_POST['ocupacionPadre'];
+    echo $TrabajoDelPadre = $_POST['TrabajoDelPadre'];
+    echo $NombreDelaMadre = $_POST['NombreDelaMadre'];
+    echo $ocupacionMadre = $_POST['ocupacionMadre'];
+    echo $TrabajoDelaMadre = $_POST['TrabajoDelaMadre'];
+    echo $Tutor = $_POST['Tutor'];
+    echo $ocupacionTutor = $_POST['ocupacionTutor'];
+    echo $TrabajoDelTutor = $_POST['TrabajoDelTutor'];
+    echo $Padre = $_POST['Padre'];
+    echo $Madre = $_POST['Madre'];
+    echo $Tutor = $_POST['Tutor'];
+    echo $Ingresos = $_POST['Ingresos'];
+    echo $Email = $_POST['Email'];
+    echo $Telefono = $_POST['Telefono'];
+    echo $InformacionPadre = $_POST['InformacionPadre'];
+    echo $InformacionMadre = $_POST['InformacionMadre'];
+    echo $InformacionTutor = $_POST['InformacionTutor'];
+    echo $Informacion = $_POST['Informacion'];
 ?>
