55817314847ed45fa9bdf8a0d8bdf047abceb641
DiegoPizanoL <67017096+DiegoPizanoL@users.noreply.github.com>
2020-06-25 17:13:48 -0500

    insertAplication.php
    Terminar la validación de los datos

--- insertAplication.php
+++ insertAplication.php
@@ -9,10 +9,10 @@
     echo $numero = isset($_POST['Numero']) ? $_POST['Numero'] : null;
   
     echo $coloniaAlumno = $_POST['ColoniaAlumno'] . '<br>';
-    echo $localidadAlumno = $_POST['LocalidadAlumno'] . '<br>';
+    echo $localidadAlumno = $_POST['Localidad'] . '<br>';
 
     //* ---------------- Información de la Escuela -----------------------------------
-    echo $nivel = $_POST['Nivel'] . '<br>';
+    echo $Nivel = isset($_POST['Nivel']) ? $_POST['Nivel'] : "P" . "<br>";
     echo $grado = $_POST['GradoEscolar'] . '<br>';
     echo $promedio = $_POST['PromedioAlumno'] . '<br>';
     echo $idEscuela = $_POST['Escuela'] . '<br>';
@@ -23,6 +23,21 @@
     echo $padre = isset($_POST['NombreDelPadre']) ? $_POST['NombreDelPadre'] : null;
     echo $ocupacionPadre = isset($_POST['ocupacionPadre']) ? $_POST['ocupacionPadre'] : null;
     echo $trabajoPadre = isset($_POST['TrabajoDelPadre']) ? $_POST['TrabajoDelPadre'] : null;
+
+    echo $Madre = isset($_POST['NombreDelaMadre']) ? $_POST['NombreDelaMadre'] : null;
+    echo $ocupacionMadre = isset($_POST['ocupacionMadre']) ? $_POST['ocupacionMadre'] : null;
+    echo $trabajoDelaMadre = isset($_POST['TrabajoDelaMadre']) ? $_POST['TrabajoDelaMadre'] : null;
+
+    echo $Tutor = isset($_POST['Tutor']) ? $_POST['Tutor'] : null;
+    echo $ocupacionTutor = isset($_POST['ocupacionTutor']) ? $_POST['ocupacionTutor'] : null;
+    echo $trabajoTutor = isset($_POST['TrabajoDelTutor']) ? $_POST['TrabajoDelTutor'] : null;
+
+    echo $Ingresos = isset($_POST['Ingresos']) ? $_POST['Ingresos'] : null;
+    echo $email = isset($_POST['Email']) ? $_POST['Email'] : null;
+    echo $Telefono = isset($_POST['Telefono']) ? $_POST['Telefono'] : null;
+    echo $TelefonoAdicional = isset($_POST['TelefonoAdicional']) ? $_POST['TelefonoAdicional'] : null;
+    echo $QuienDaInformacion = isset($_POST['QuienDaInformacion']) ? $_POST['QuienDaInformacion'] : "M" . "<br>";
+    echo $Informacion = isset($_POST['Informacion']) ? $_POST['Informacion'] : null;
     
     /*
     if (isset($_POST['NombreDelaMadre'])){
