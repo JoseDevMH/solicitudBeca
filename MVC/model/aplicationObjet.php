8680c25723a4a9e4a379080dfd4a936469e09a41
Jose Garcia <josegarciaalvarez81@gmail.com>
2020-06-30 00:06:06 -0500

    atributos de clase aplicationObjet

---
+++ MVC/model/aplicationObjet.php
@@ -0,0 +1,39 @@
+<?php   
+
+    class aplicationObjet
+    {
+        private $id;
+        private $name;
+        private $firstLastName;
+        private $secondLastName;
+        private $curp;
+        private $age;
+        private $address;
+        private $suburb;
+        private $idLocality;
+        private $phone;
+        private $phone2;
+        private $email;
+        private $level;
+        private $grade;
+        private $score;
+        private $idSchool;
+        private $dadName;
+        private $dadWorkplace;
+        private $dadWorkplaceAddress;
+        private $momName;
+        private $momWorkplace;
+        private $momWorkplaceAddress;
+        private $tutorName;
+        private $tutorWorkplace;
+        private $tutorWorkplaceAddress;
+        private $sign;
+        private $signName;
+        private $doccurp;
+        private $proofAddress;
+        private $docScore;
+        private $status;
+
+    }
+    
+?>
