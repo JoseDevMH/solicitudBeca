2bb9c2890d7b9e45c2590d7a81392bf9ec95b342
Jose Garcia <josegarciaalvarez81@gmail.com>
2020-07-07 09:38:29 -0500

    errores corregidos list ang get aplications

--- MVC/view/getAplication.php
+++ MVC/view/getAplication.php
@@ -26,17 +26,17 @@
                 $nombre = $item->getName();
                 $primerApellido = $item->getFirstLastName();
                 $segundoApellido = $item->getSecondLastName();
-                $Edad = $item->getEdadAlumno();
+                $edad = $item->getAge();
                 $curp = $item->getCurp();
-                $Calle = $item->getCalle();
-                $Numero = $item->getNumero();
-                $Colonia = $item->getColoniaAlumno();
-                $Nivel = $item->getIdNivel();
-                $Grade = $item->getIdGrado();
-                $Score = $item->getIdPromedio();
+                $calle = $item->getStreet();
+                $numero = $item->getNumber();
+                $colonia = $item->getSuburb();
+                $nivel = $item->getLevel();
+                $grado = $item->getGrade();
+                $promedio = $item->getScore();
                 $escuela = $item->getIdSchool();
                 $localidad = $item->getIdLocality();
-                $Income = $item->getIdIngresos();
+                $ingreso = $item->getIncome();
                 
             
             }
@@ -48,81 +48,55 @@
                         <h3>Información del Alumno</h3>
                         </div>
                         <div class="card-body">
-                        <div class="row">
-                            <div class="col-12 ">
-                            <div class="col-12 ">
-                            <div class="form-group inline-block">
+                        <div class="row">                            
+                            <div class="col-md-6">
+                            
                                 <label class="font-weight-bold" for="nombrerA">Nombre: </label>
-                                <?php echo $nombre; ?>
+                                <?php echo $nombre . ' ' , $primerApellido . ' ' . $segundoApellido; ?>
+                            
                             </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label class="font-weight-bold" for="priApellido">Primer Apellido: </label>
-                                <?php echo $primerApellido; ?>
-                            </div>
-                            </div>
-                            <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label class="font-weight-bold" for="segApellido">Segundo Apellido: </label>
-                                <?php echo $segundoApellido; ?>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3">
-                            <div class="form-group">
-                                <label class="font-weight-bold" for="edadA">Edad: </label>
-                                <?php echo $Edad; ?>
-                            </div>
-                            </div>
-                            <div class="col-md-9">
-                            <div class="form-group">
+
+                            <div class="col-md-4">
+                            
                                 <label class="font-weight-bold" for="curp">CURP: </label>
                                 <?php echo $curp; ?>
+                            
                             </div>
+
+                            <div class="col-md-2">
+                            
+                                <label class="font-weight-bold" for="edadA">Edad: </label>
+                                <?php echo $edad; ?>
+                            
                             </div>
+                        </div>                       
+                        <div class="row">
+                            
+                            
                         </div>
                         <div class="row">
-                            <div class="form-group col-9">
-                            <label class="font-weight-bold" for="calle">Calle: </label>
-                            <?php echo $Calle; ?>
-                            </div>
-                            <div class="form-group col-3">
-                            <label class="font-weight-bold" for="num_exA">Número Exterior: </label>
-                            <?php echo $Numero; ?>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="form-group col-md-6">
-                            <label class="font-weight-bold" for="colA">Colonia:</label>
-                            <?php echo $Colonia; ?>
+                            <div class="col-12">
+                            <label class="font-weight-bold" for="calle">Domicilio: </label>
+                            <?php echo $calle . ' ' . $numero . ' ' . $colonia . ' ' . $localidad; ?>
                             </div>
                             
-                            <div class="form-group col-md-6">
-                            <label class="font-weight-bold" for="localA">Localidad: </label>
-                            <?php echo $localidad; ?>
-                                <!--//fin del select ////////////////////////////-->              
-                            </div>
                         </div>
                         <div class="row">
                             <div class="form-group col-3">
                             <label class="font-weight-bold" for="calle">Nivel: </label>
-                            <?php echo $Nivel; ?>
+                            <?php echo $nivel; ?>
                             </div>
                             <div class="form-group col-3">
                             <label class="font-weight-bold" for="num_exA">Grado: </label>
-                            <?php echo $Grade; ?>
+                            <?php echo $grado; ?>
                             </div>
                             <div class="form-group col-3">
                             <label class="font-weight-bold" for="num_exA">Promedio: </label>
-                            <?php echo $Score; ?>
+                            <?php echo $promedio; ?>
                             </div>
                             <div class="form-group col-3">
                             <label class="font-weight-bold" for="num_exA">Ingresos: </label>
-                            <?php echo $Income; ?>
+                            <?php echo $ingreso; ?>
                             </div>
                         </div>
                         </div>
--- MVC/view/listAplications.php
+++ MVC/view/listAplications.php
@@ -11,15 +11,11 @@
         <table class="table table-striped table-dark">
         <thead>
             <tr>            
-            <th scope="col">Id</th>
             <th scope="col">Fecha</th>
             <th scope="col">Nombre</th>
-            <th scope="col">Primer Apellido</th>
-            <th scope="col">Segundo Apellido</th>
             <th scope="col">Edad</th>
             <th scope="col">CURP</th>
-            <th scope="col">Calle</th>
-            <th scope="col">Número Exterior</th>
+            <th scope="col">Domicilio</th>          
             <th scope="col">Nivel</th>
             <th scope="col">Grado</th>
             <th scope="col">Promedio</th>
@@ -42,23 +38,17 @@
                 foreach ($listAplications as $item){
                 
                 echo "<tr>";
-
-                echo '<th scope="row">'. $item->getId() . '</th>';
                 echo '<td>' . $item->getDateAplication() . '</td>'; 
-                echo '<td>' . $item->getName() . '</td>';
-                echo '<td>' . $item->getFirstLastName() . '</td>';
-                echo '<td>' . $item->getSecondLastName() . '</td>';
-                echo '<td>' . $item->getEdadAlumno() . '</td>';
+                echo '<td>' . $item->getName() . ' ' . $item->getFirstLastName() . ' ' . $item->getSecondLastName() . '</td>';            
+                echo '<td>' . $item->getAge() . '</td>';
                 echo '<td>' . $item->getCurp() . '</td>';
-                echo '<td>' . $item->getCalle() . '</td>';
-                echo '<td>' . $item->getNumero() . '</td>';
-                echo '<td>' . $item->getColoniaAlumno() . '</td>';
-                echo '<td>' . $item->getIdNivel() . '</td>';
-                echo '<td>' . $item->getIdGrado() . '</td>';
-                echo '<td>' . $item->getIdPromedio() . '</td>';
-                echo '<td>' . $item->getIdSchool() . '</td>';
-                echo '<td>' . $item->getIdLocality() . '</td>';
-                echo '<td>' . $item->getIdIngresos() . '</td>';
+                echo '<td>' . $item->getStreet() . ' ' . $item->getNumber() . ' ' .$item->getSuburb(). '</td>';
+                echo '<td>' . $item->getLevel() . '</td>';
+                echo '<td>' . $item->getGrade() . '</td>';
+                echo '<td>' . $item->getScore() . '</td>';
+                echo '<td>' . $item->getIdSchool() . '</td>';               
+                echo '<td>' . $item->getIdLocality() . '</td>';                
+                echo '<td>' . $item->getIncome() . '</td>';
                 echo '<td><a class="linkNaranja" href="getAplication.php?Id=' . $item->getId() . '"> Ver más </a></td>';
                 echo "</tr>";
                
